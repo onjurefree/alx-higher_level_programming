ab6414c531aa189ba948c524d0a8fe5754017b24
onjurefree <fridahonjushs@gmail.com>
2023-10-02 00:08:47 -0700

    ./9-easter_egg.py

---
+++ 0x00-python-hello_world/2-print.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+number = 98
+print("\"Programming is like building a multilingual puzzele"
---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str1 = "Holberton"
+print(3 * str)
+print(str[:9])
